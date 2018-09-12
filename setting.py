8f85a34b0f99d309c2e0cb33eb3a6346760b4871
dick0v3rs3a <35269972+dick0v3rs3a@users.noreply.github.com>
2018-09-12 12:51:13 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,3 +1,3 @@
-token = 'a6a149ae036ad8d42ce60c03dcb989dbeafdb80dfa9e3d2e65a62989b76e33de69ec8fe7425b335b68151'
-confirmation_token = '8296940a'
-access_token = '86da8bf525775e1d3f03ac7a490438cbc5c53871c2c16294f6a42e03ab672cda1e340f376a88de1d0f6ec'
+token = 'a6a149ae036ad8d42ce60c03dcb989dbeafdb80dfa9e3d2e65a62989b76e33de69ec88151'
+confirmation_token = '820a'
+access_token = '86da8bf525775e1d3f03ac7a490438cbc5c53871c2c162942cda1e340f376a88de1d0f6ec'
